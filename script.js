4309f9f063fa8cdd55eb42012fad96fa01ece136
abid982 <abidarif982@gmail.com>
2023-10-06 17:07:18 +0500

    Add event listener to roll dice button, generate random number, display dice according to random number, accumulate total score if the dice is not 1

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 'use strict';
+
+
+// When page refresh or first time website open
+// DOM Elements
+const newButton = document.querySelector('.btn--new');
+const rollButton = document.querySelector('.btn--roll');
+const holdButton = document.querySelector('.btn--hold');
+const diceElement = document.querySelector('.dice');
+
+console.log(newButton);
+console.log(rollButton);
+console.log(holdButton);
+
+diceElement.style.display = 'none';
+
+
+// 1 and 20
+// Math.trunc(Math.random() * 20) + 1
+
+let sum = 0;
+
+
+rollButton.addEventListener('click', function () {
+    // debugger;
+    console.log('Roll button was clicked!');
+
+    // Generate random number between 1 and 6
+    // Math.trunc(Math.random() * 6 ) + 1 --> 1 and 6
+    const randomNumber = Math.trunc(Math.random() * 6) + 1;
+    console.log(randomNumber);
+
+    // dice-1.png
+    // File name generate
+    const dice = `dice-${randomNumber}.png`;
+    console.log(dice);
+
+    // Change image src
+    document.querySelector('.dice').src = dice;
+    diceElement.style.display = 'block';
+
+    console.log(typeof randomNumber);
+
+    // Check is random number 1
+    if (randomNumber === 1) {
+        // Switch next player
+    } else {
+        sum = sum + randomNumber;
+        console.log(sum);
+
+        document.querySelector('#current--0').textContent = sum;
+    }
+});
